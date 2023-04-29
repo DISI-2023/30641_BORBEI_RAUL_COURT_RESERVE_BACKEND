1b8edc3d5f265b9fd8471db36966aaa1bd6b5071
Raul Borbei <borbeiraul@yahoo.com>
2023-04-29 18:13:55 +0300

    fixed field and location CRUDS to take null entries

--- src/main/java/org/example/services/FieldService.java
+++ src/main/java/org/example/services/FieldService.java
@@ -31,15 +31,17 @@ public class FieldService {
     private final TariffRepository tariffRepository;
 
     @Autowired
-    public FieldService (FieldRepository fieldRepository, LocationRepository locationRepository, TariffRepository tariffRepository){
+    public FieldService(FieldRepository fieldRepository, LocationRepository locationRepository, TariffRepository tariffRepository) {
         this.fieldRepository = fieldRepository;
         this.locationRepository = locationRepository;
         this.tariffRepository = tariffRepository;
     }
 
     /** CREATE **/
-    /** tested **/
-    public UUID insert(FieldDetailsDTO dto){
+    /**
+     * tested
+     **/
+    public UUID insert(FieldDetailsDTO dto) {
         //location validation
         Location newLocation = this.validateLocation(dto.getLocationId());
 
@@ -53,7 +55,7 @@ public class FieldService {
         return field.getId();
     }
 
-    private void createDefaultTariffs(Field field, String type){
+    private void createDefaultTariffs(Field field, String type) {
         Tariff newTariff = new Tariff();
         newTariff.setField(field);
         newTariff.setPrice(0);
@@ -62,24 +64,30 @@ public class FieldService {
     }
 
     /** SELECT **/
-    /** tested **/
-    public List<FieldAndLocationDetailsDTO> findAll(){
+    /**
+     * tested
+     **/
+    public List<FieldAndLocationDetailsDTO> findAll() {
         List<Field> fields = fieldRepository.findAll();
         return fields.stream().map(FieldBuilder::toFieldAndLocationDetailsDTO).collect(Collectors.toList());
     }
 
-    /** tested **/
-    public FieldDetailsDTO findById(UUID id){
+    /**
+     * tested
+     **/
+    public FieldDetailsDTO findById(UUID id) {
         Optional<Field> field = fieldRepository.findById(id);
-        if (!field.isPresent()){
+        if (!field.isPresent()) {
             LOGGER.error("Field with id {} was not found in db", id);
             throw new ResourceNotFoundException(Field.class.getSimpleName());
         }
         return FieldBuilder.toFieldDetailsDTO(field.get());
     }
 
-    /** tested **/
-    public List<FieldDetailsDTO> findByLocationId(UUID locationId){
+    /**
+     * tested
+     **/
+    public List<FieldDetailsDTO> findByLocationId(UUID locationId) {
         Location location = this.validateLocation(locationId);
 
         List<Field> fields = fieldRepository.findByLocation(location);
@@ -88,30 +96,36 @@ public class FieldService {
     }
 
     /** UPDATE **/
-    /** tested **/
-    public UUID update(FieldDetailsDTO dto){
+    /**
+     * tested
+     **/
+    public UUID update(FieldDetailsDTO dto) {
         UUID id = dto.getId();
 
         Optional<Field> field = fieldRepository.findById(id);
-        if (!field.isPresent()){
+        if (!field.isPresent()) {
             LOGGER.error("Field with id {} was not found in db", id);
             throw new ResourceNotFoundException(Field.class.getSimpleName());
         }
+        if (dto.getLocationId() != null) {
+            Location newLocation = this.validateLocation(dto.getLocationId());
+            field.get().setLocation(newLocation);
+        }
+        if (dto.getName() != null)
+            field.get().setName(dto.getName());
 
-        Location newLocation = this.validateLocation(dto.getLocationId());
-
-        field.get().setName(dto.getName());
-        field.get().setLocation(newLocation);
         Field updatedField = fieldRepository.save(field.get());
         LOGGER.debug("Field with id {} was updated in db", id);
         return id;
     }
 
     /** DELETE **/
-    /** tested **/
-    public void delete(UUID id){
+    /**
+     * tested
+     **/
+    public void delete(UUID id) {
         Optional<Field> field = fieldRepository.findById(id);
-        if (!field.isPresent()){
+        if (!field.isPresent()) {
             LOGGER.error("Field with id {} was not found in db", id);
             throw new ResourceNotFoundException(Field.class.getSimpleName());
         }
@@ -119,13 +133,14 @@ public class FieldService {
         LOGGER.debug("Field with id {} was deleted", id);
     }
 
-    /** This method checks if a location ID (coming usually from a DTO)
-     *  is valid and can be found in the DB and if it is, the method will
-     *  return the Location object (to be added to the entity later on)
+    /**
+     * This method checks if a location ID (coming usually from a DTO)
+     * is valid and can be found in the DB and if it is, the method will
+     * return the Location object (to be added to the entity later on)
      */
-    private Location validateLocation(UUID locationId){
+    private Location validateLocation(UUID locationId) {
         Optional<Location> location = locationRepository.findById(locationId);
-        if (!location.isPresent()){
+        if (!location.isPresent()) {
             LOGGER.error("Location with id {} was not found in db", locationId);
             throw new ResourceNotFoundException(Field.class.getSimpleName());
         }
